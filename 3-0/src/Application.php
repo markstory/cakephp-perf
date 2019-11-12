c34151fcfb4f80d18eb0a346b487d2a755131f4c
Mark Story <mark@mark-story.com>
2019-11-11 22:50:55 -0500

    Add consol shell and remove debugkit

--- 3-0/src/Application.php
+++ 3-0/src/Application.php
@@ -41,14 +41,6 @@ class Application extends BaseApplication
             $this->bootstrapCli();
         }
 
-        /*
-         * Only try to load DebugKit in development mode
-         * Debug Kit should not be installed on a production system
-         */
-        if (Configure::read('debug')) {
-            $this->addPlugin(\DebugKit\Plugin::class);
-        }
-
         // Load more plugins here
     }
 
